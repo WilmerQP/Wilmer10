65497665718d5e4c1bad97a26b678ef9045c7bb2
Wilmer Danilo Quitio Piltaxi <wd.quitiop@uea.edu.ec>
2024-03-24 10:53:43 -0500

    Información_Personal

---
+++ Información_Personal.py
@@ -0,0 +1,13 @@
+# Creación del diccionario con información personal
+informacion_personal = {
+    "nombre": "Wilmer Quitio",
+    "edad": 28,
+    "ciudad": "Ciudad de Riobamba",
+    "profesion": "Estuadiante"
+}
+
+# Acceso a la información del diccionario
+print("Nombre:", informacion_personal["nombre"])
+print("Edad:", informacion_personal["edad"])
+print("Ciudad:", informacion_personal["ciudad"])
+print("Profesión:", informacion_personal["profesion"])
